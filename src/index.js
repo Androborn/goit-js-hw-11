f22c1ec1417762dade300ad4e3010c2654c499f4
androborn <andrii.titov@gmail.com>
2021-11-28 16:20:19 +0200

    Fix image breaking grid row heigth
    *Refactor img-card markcup
    *Refactor classes
    *Add relative positioning to images inside the card

--- src/index.js
+++ src/index.js
@@ -135,16 +135,14 @@ function addCardsMarkup(fatchedImages) {
 
   const markup = fatchedImages.hits
     .map(image => {
-      return `<a href="${image.largeImageURL}">
-    <div class="photo-card">
-      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      return `<a class="photo-card" href="${image.largeImageURL}">
+      <img class="thumb-img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
       <div class="info">
         <p class="info-item"><b>Likes</b> ${image.likes}</p>
         <p class="info-item"><b>Views</b> ${image.views}</p>
         <p class="info-item"><b>Comments</b> ${image.comments}</p>
         <p class="info-item"><b>Downloads</b> ${image.downloads}</p>
       </div>
-    </div>
   </a>`;
     })
     .join('');
